82d59871e928f29c40f4a739ce084cb7f6dd6dbb
TashrinTanisha <tashrin.tarannum@gmail.com>
2023-09-18 22:12:41 +0600

    New request done

--- sample.c
+++ sample.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int num1,num2,sum;
+    int num1;
+    int num2;
+    int sum;
 
-    printf("Enter num1 & num2 : ");
-    scanf("%d %d",&num1,&num2);
+    printf("Enter num1 : ");
+    printf("Enter num2 : ");
+
+    scanf("%d",&num1);
+    scanf("%d",&num2);
 
     sum = num1 + num2;
 
