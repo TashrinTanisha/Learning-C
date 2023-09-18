2f015807cc96971206d0d461e30a2d0584258a26
TashrinTanisha <tashrin.tarannum@gmail.com>
2023-09-19 00:41:16 +0600

    variable-3 done

---
+++ variable-3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    float x;
+
+    x = 5.5;
+
+    float y = 4.4;
+
+    printf("Addition :%f\n",(x+y));
+
+    int m,k,n;
+
+    m = 3;
+
+    k = 4;
+
+    n = m * k;
+
+    printf("%d",n);
+
+    return 0;
+}
