f58daab2fdaee47d443ef425c8fc15456e8248df
TashrinTanisha <tashrin.tarannum@gmail.com>
2023-09-19 16:03:58 +0600

    ASCII value-character done

---
+++ ASCII value-character.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int value;
+
+    printf("Enter any ASCII value : ");
+    scanf("%d",&value);
+
+    printf("The ASCII character is : %c",value);
+
+    return 0;
+}
