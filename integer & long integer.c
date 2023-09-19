861d79a1d0ba570332bbb347ff9d08c4036695f6
TashrinTanisha <tashrin.tarannum@gmail.com>
2023-09-19 14:09:39 +0600

    integer & long integer done

---
+++ integer & long integer.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+    printf("The minimum value of INT : %d\n",INT_MIN);
+    printf("The maximum value of INT : %d\n",INT_MAX);
+
+    printf("The minimum value of LONG : %ld\n",LONG_MIN);
+    printf("The maximum value of LONG : %ld",LONG_MAX);
+
+    return 0;
+}
