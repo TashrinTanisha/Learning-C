06912349baddda040ffd2f1fa838c7f6d01af97c
TashrinTanisha <tashrin.tarannum@gmail.com>
2023-09-20 13:10:57 +0600

    Hexa-decimal to decimal done

---
+++ Hexa-decimal to decimal.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int hexadecimal;
+
+    printf("Hexa-decimal number : ");
+    scanf("%x",&hexadecimal);
+
+    printf("Decimal number : %d",hexadecimal);
+
+    return 0;
+}
