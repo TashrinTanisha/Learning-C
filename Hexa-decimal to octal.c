8bb2146d1a40a8a33c431f806a60b906ffb283af
TashrinTanisha <tashrin.tarannum@gmail.com>
2023-09-20 13:31:49 +0600

    Hexa-decimal to octal done

---
+++ Hexa-decimal to octal.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int hexadecimal;
+
+    printf("Hexa-decimal number : ");
+    scanf("%x",&hexadecimal);
+
+    printf("Octal number : %o",hexadecimal);
+
+    return 0;
+}
