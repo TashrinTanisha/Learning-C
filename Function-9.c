93b1350c2c2a673969a3a08381102ff069a2e829
Tashrin Tanisha Ishq <145369121+TashrinTanisha@users.noreply.github.com>
2024-05-02 16:10:30 +0600

    Function-9 is completed

---
+++ Function-9.c
@@ -0,0 +1,20 @@
+/*
+   Write a function that simply prints a message "Hello!! I'm from inside of a function".
+   Call this function from the main function
+*/
+
+#include<stdio.h>
+
+void message()
+{
+
+    printf("Hello!! I'm from inside of a function\n");
+
+}
+
+int main()
+{
+    message() ;
+
+    return 0;
+}
