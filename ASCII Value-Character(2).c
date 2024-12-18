5dec841bb1709838fdf5d4cb348d6848d722570f
Tashrin Tanisha Ishq <145369121+TashrinTanisha@users.noreply.github.com>
2024-12-19 00:42:34 +0600

    Completed

---
+++ ASCII Value-Character(2).c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int value ;
+
+    printf("Enter the ASCII Value : ");
+    scanf("%d", &value);
+
+    printf("The ASCII Character is : %c", value);
+    return 0;
+}
