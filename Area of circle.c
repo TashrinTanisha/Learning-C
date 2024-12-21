554957df6df9c070efe474e06eb18f6bbc405cc7
Tashrin Tarannum Tanisha Ishq <145369121+TashrinTanisha@users.noreply.github.com>
2024-12-22 01:09:29 +0600

    Completed

--- Area of circle.c
+++ Area of circle.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
-#include<math.h>
 int main()
 {
-    double radius,area;
+    double radius, area ;
 
-    printf("Enter radius : ");
-    scanf("%lf",&radius);
+    printf("Enter radius :");
+    scanf("%lf", &radius);
 
-    area = 3.1416 * radius * radius;
+    area = 3.1416 * radius * radius ;
 
-    printf("Area of circle : %.3lf",area);
+    printf("The area is : %.3lf", area);
 
     return 0;
 }
