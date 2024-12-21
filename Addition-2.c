0634851f51e66ee3443854042c21612fdb8e8b23
Tashrin Tarannum Tanisha Ishq <145369121+TashrinTanisha@users.noreply.github.com>
2024-12-21 20:32:30 +0600

    Completed

--- Addition-2.c
+++ Addition-2.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int num1,num2,sum;
+    int num1, num2;
 
-    printf("Enter num1 & num2 : ");
-    scanf("%d %d",&num1,&num2);
+    printf("Enter num1 & num2 : ") ;
+    scanf("%d %d", &num1, &num2);
 
-    sum = num1 + num2;
+    int sum = num1 + num2 ;
 
-    printf("Sum : %d",sum);
+    printf("The sum is : %d",sum);
 
     return 0;
 }
