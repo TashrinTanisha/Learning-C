8f119897e27c070000453e1fed604858c2ac9e0d
Tashrin Tarannum Tanisha Ishq <145369121+TashrinTanisha@users.noreply.github.com>
2025-01-07 17:12:46 +0600

    Completed

---
+++ Input-Output assignment-1.c
@@ -0,0 +1,15 @@
+//Write a program to print your name and details
+
+#include<stdio.h>
+int main()
+{
+    printf("My name is Tashrin Tarannum Tanisha Ishq\n");
+
+    printf("I read in class 9\n");
+
+    printf("I am from Bangladesh\n");
+
+    printf("My hobbies are programming, listening to music, singing etc.");
+
+    return 0;
+}
