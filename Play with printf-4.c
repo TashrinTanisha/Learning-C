a09f90b5ad3050826de4caa655ec4bb921e0a1b4
Tashrin Tarannum Tanisha Ishq <145369121+TashrinTanisha@users.noreply.github.com>
2025-01-11 15:18:53 +0600

    Done

---
+++ Play with printf-4.c
@@ -0,0 +1,11 @@
+/* Write a C program to print the following output:
+   Name:    Tanisha
+   Country:    Bangladesh */
+#include<stdio.h>
+int main()
+{
+    printf("Name:\tTanisha\n");
+    printf("Country:\tBangladesh");
+
+    return 0;
+}
