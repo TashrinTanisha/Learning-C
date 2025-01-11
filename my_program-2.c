b9f36fbb4ed407161ad1ed1ac0e4af881e85c268
Tashrin Tarannum Tanisha Ishq <145369121+TashrinTanisha@users.noreply.github.com>
2025-01-11 15:17:37 +0600

    Done

---
+++ my_program-2.c
@@ -0,0 +1,17 @@
+/* Create a file and save it giving a name .c extension.
+   (my_program.c)*/
+//Write the skeleton or the main structure of a C program
+//Print something inside the program
+//Compile and run the program
+
+#include<stdio.h>
+int main()
+{
+    printf("My name is Tashrin Tarannum Tanisha\n");
+
+    printf("I am 14 years old\n");
+
+    printf("I read in class 9\n");
+
+    return 0;
+}
