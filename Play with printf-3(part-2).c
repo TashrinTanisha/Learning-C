3fe0f9f82b14194e4ce8f3637a8718933e0f62ab
Tashrin Tarannum Tanisha Ishq <145369121+TashrinTanisha@users.noreply.github.com>
2025-01-11 15:18:30 +0600

    Completed

---
+++ Play with printf-3(part-2).c
@@ -0,0 +1,9 @@
+// To print a new line use "\n" inside printf() function
+// To print a tab use "\t" inside printf() function
+#include<stdio.h>
+int main()
+{
+    printf("Name: Ishq\nCountry:(Chittagong)\tBangladesh");
+
+    return 0;
+}
