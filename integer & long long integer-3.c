7c31a964691fb8d55a24c34391625bedbbecb03c
Tashrin Tarannum Tanisha <145369121+TashrinTanisha@users.noreply.github.com>
2025-08-02 23:59:57 +0600

    Finished

---
+++ integer & long long integer-3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+    printf("The minimum value of integer is: %d\n", INT_MIN);
+    printf("The maximum value of integer is: %d\n", INT_MAX);
+
+    printf("The minimum value of long integer is: %lld\n", LLONG_MIN);
+    printf("The maximum value of long integer is: %lld\n", LLONG_MAX);
+
+    return 0;
+}
