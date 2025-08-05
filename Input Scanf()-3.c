2e45aaadb5ff1c7131dc97d3c1cbf28fe077c79b
Tashrin Tarannum Tanisha <145369121+TashrinTanisha@users.noreply.github.com>
2025-08-05 17:43:57 +0600

    Input Scanf()-3.c is finished

---
+++ Input Scanf()-3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float x, y;
+
+
+    printf("Enter the value of x & y: ");
+    scanf("%f %f", &x, &y);
+
+    printf("You entered: %.2f & %.2f\n", x, y);
+
+    return 0;
+}
